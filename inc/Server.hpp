1d1a7611ed833db1f9b4255e7c74ae033e0b293b
hoodche <nateto@hotmail.com>
2024-12-28 12:15:27 +0100

    connections are now accepted and stored in Server::fds vector

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: igcastil <igcastil@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 18:35:03 by igcastil          #+#    #+#             */
-/*   Updated: 2024/12/26 11:20:42 by igcastil         ###   ########.fr       */
+/*   Updated: 2024/12/28 12:10:22 by igcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 
 #include <string>
 #include <netinet/in.h>
+#include <vector>
+#include <poll.h> // for struct pollfd
+
 
 class Server
 {
@@ -22,13 +25,14 @@ private:
 	
 	int port;
 	std::string password;
-	int serverSocketFd, clientSocketFd;
+	int listenSocketFd;
 	struct sockaddr_in serverAddress, clientAddress;	//struct sockaddr_in {
 														//	sa_family_t     sin_family;     / is an unsigned short int
 														//	in_port_t       sin_port;       / is an unsigned short int
 														//	struct in_addr  sin_addr;       / IPv4 address */
 														//}; handles IPv4 addresses The sin_port and sin_addr members are stored in network byte order.
-	//struct pollfd new_cli;	//struct pollfd {
+	std::vector<struct pollfd> fds;//this vector holds all socket fds to be polled (the first one is server's listen socket -polled for incoming connections- and the rest are client connected sockets returned by the call to accept -polled for incoming data-)
+	struct pollfd connectedSocket;	//struct pollfd {
 								//					int fd;
 								//					short events; (common event flags include POLLIN: Data other than high-priority data can be read without blocking. POLLOUT: Normal data can be written without blocking. POLLERR: An error has occurred on the file descriptor.
 								//					short revents; (output parameter, filled by the kernel with the events that actually occurred)
@@ -39,6 +43,7 @@ public:
 	void initSocket();
 	void init(int port, std::string pass);
 	static void SignalHandler(int signum);
+	void accept_new_client();
 };
 
 #endif
--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: igcastil <igcastil@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 18:26:33 by igcastil          #+#    #+#             */
-/*   Updated: 2024/12/26 11:43:49 by igcastil         ###   ########.fr       */
+/*   Updated: 2024/12/28 12:13:39 by igcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@
 #include <iostream>
 #include <unistd.h> // for read
 #include <csignal> //for SIGINT and SIGQUIT
+#include <string.h> //for memset
 
-Server::Server() : serverSocketFd(-1), clientSocketFd(-1)
+Server::Server() : listenSocketFd(-1)
 {
 }
 bool Server::signalReceived = false;
@@ -34,10 +35,8 @@ void Server::SignalHandler(int signal)
 
 void	Server::closeFds()
 {
-	if (this->serverSocketFd != -1)
-		close(this->serverSocketFd);
-	if (this->clientSocketFd != -1)
-		close(this->clientSocketFd);
+	for (size_t i = 0; i < this->fds.size(); i++)
+		close(this->fds[i].fd);
 }
 
 void Server::initSocket()
@@ -48,19 +47,23 @@ void Server::initSocket()
 													//from host byte order (endianness) to network byte order. In most systems, the host byte order is little-endian,
 													//however, network protocols like TCP/IP use big-endian order.
 	this->serverAddress.sin_addr.s_addr = INADDR_ANY; //This macro allows the socket to bind to all available interfaces on the host machine. It is typically used in server applications to accept connections from any IP address.
-	this->serverSocketFd = socket(AF_INET, SOCK_STREAM, 0);	//SOCK_STREAM indicates that the socket type is stream-oriented, which means it provides a reliable, two-way, connection-based byte stream. This type of socket is typically used for TCP connections. TCP is a protocol that ensures data is delivered in the same order it was sent and without errors, making it suitable for applications that require reliable communication, such as web servers, email clients, and file transfer programs.
+	this->listenSocketFd = socket(AF_INET, SOCK_STREAM, 0);	//SOCK_STREAM indicates that the socket type is stream-oriented, which means it provides a reliable, two-way, connection-based byte stream. This type of socket is typically used for TCP connections. TCP is a protocol that ensures data is delivered in the same order it was sent and without errors, making it suitable for applications that require reliable communication, such as web servers, email clients, and file transfer programs.
 														//3rd arg is the protocol to be used with the socket. Typically, this is set to 0 to use the default protocol for the specified address family (1st arg) and socket type (2nd arg)
-	if(serverSocketFd == -1)
+	if(listenSocketFd == -1)
 		throw(std::runtime_error("socket could not be created "));
-	if(setsockopt(serverSocketFd, SOL_SOCKET, SO_REUSEADDR, &socketOptionEnable, sizeof(socketOptionEnable)) == -1)//allows to customize the behavior of a socket. 2nd arg specifies the protocol level at which the option resides (common values include SOL_SOCKET for socket-level options and IPPROTO_TCP for TCP-level options). 3rd arg specifies the option to be set (SO_REUSEADDR to allow reuse of local addresses)
+	if(setsockopt(listenSocketFd, SOL_SOCKET, SO_REUSEADDR, &socketOptionEnable, sizeof(socketOptionEnable)) == -1)//allows to customize the behavior of a socket. 2nd arg specifies the protocol level at which the option resides (common values include SOL_SOCKET for socket-level options and IPPROTO_TCP for TCP-level options). 3rd arg specifies the option to be set (SO_REUSEADDR to allow reuse of local addresses)
 		throw(std::runtime_error("could not set option SO_REUSEADDR to allow reuse of local addresses for socket"));
-	//if (fcntl(serverSocketFd, F_SETFL, O_NONBLOCK) == -1)//The fcntl function is used to perform various operations on file descriptors, including sockets. One common use of fcntl is to set a socket to non-blocking mode, which allows the program to continue executing without waiting for the socket operations to complete.
-	//	throw(std::runtime_error("could not set option O_NONBLOCK for socket"));
-	if (bind(serverSocketFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)//The bind function is used to associate a socket with a specific local address and port number. 2nd arg is a pointer to a sockaddr structure that contains the address to which the socket should be bound. 
+	if (fcntl(listenSocketFd, F_SETFL, O_NONBLOCK) == -1)//The fcntl function is used to perform various operations on file descriptors, including sockets. One common use of fcntl is to set a socket to non-blocking mode, which allows the program to continue executing without waiting for the socket operations to complete. When this is set, calls to accept() will return -1 immediately (also read and write on the socket will return immediately if there is no data available, rather than blocking until data is received or sent.)
+		throw(std::runtime_error("could not set option O_NONBLOCK for socket"));
+	if (bind(listenSocketFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)//The bind function is used to associate a socket with a specific local address and port number. 2nd arg is a pointer to a sockaddr structure that contains the address to which the socket should be bound. 
 		throw(std::runtime_error("could not bind server socket to its address and port"));
-	if (listen(serverSocketFd, SOMAXCONN) == -1)//The listen function marks a socket as passive, meaning it will be used to accept incoming connection requests. 2nd arg is an int that defines the maximum length of the queue for pending connections (specifies how many connection requests can be queued up while the server is busy handling other connections)
+	if (listen(listenSocketFd, SOMAXCONN) == -1)//The listen function marks a socket as passive, meaning it will be used to accept incoming connection requests. 2nd arg is an int that defines the maximum length of the queue for pending connections (specifies how many connection requests can be queued up while the server is busy handling other connections)
 		throw(std::runtime_error("server can not listen on socket "));
 	std::cout << "Server listening on port " << this->port << std::endl;
+	this->connectedSocket.fd = listenSocketFd; // listenSocketFd is the first fd in the vector, since it must be polled for incoming connections which will be accepted and added to the vector as a new connected socket
+	this->connectedSocket.events = POLLIN;
+	this->connectedSocket.revents = 0;
+	this->fds.push_back(this->connectedSocket);//adds a deep copy of the pollfd struct to the vector (so this connectedSocket pollfd struct is reused -overwritten- each time a new client connects and must be added to fds vector to be polled)
 
 }
 
@@ -72,21 +75,58 @@ void Server::init(int port, std::string password)
 	this->initSocket();
 
 	//________TESTING BLOCK_________
-	char buffer[1000];
+/* 	char buffer[1000];
 	socklen_t clientAddressLength = sizeof(clientAddress);
-	this->clientSocketFd = accept(serverSocketFd, (struct sockaddr *)&clientAddress, &clientAddressLength);//Await a connection on serverSocketFd. When a connection arrives, opens a new socket to communicate with it (returning its socket descriptor or -1 for errors),sets clientAddress (which is clientAddressLength bytes long) to the address of the connecting peer and clientAddressLength to the address's actual length.
+	this->clientSocketFd = accept(listenSocketFd, (struct sockaddr *)&clientAddress, &clientAddressLength);//Await a connection on listenSocketFd. When a connection arrives, opens a new socket to communicate with it (returning its socket descriptor or -1 for errors),sets clientAddress (which is clientAddressLength bytes long) to the address of the connecting peer and clientAddressLength to the address's actual length.
 	if (this->clientSocketFd < 0)
 		throw(std::runtime_error("server could not accept incoming connection "));
-	ssize_t bytes_read = read(this->clientSocketFd, buffer, sizeof(buffer) - 1);//Reads 3rd arg bytes into buffer from clientSocketFd. Returns the number read, -1 for errors or 0 for EOF
+	ssize_t bytes_read = read(this->clientSocketFd, buffer, sizeof(buffer) - 1);//Reads 3rd arg bytes into buffer from clientSocketFd. Returns the number read, -1 for errors or 0 for EOF.The call to read() is blocking by default.(it will block the execution of the program until data is available to be read from the file descriptor or an error occurs. If there is no data available, the program will wait (block) until data becomes available.)
 	if (bytes_read < 0)
 		throw(std::runtime_error("server could not read incoming message "));
 	buffer[bytes_read] = '\0'; // Null-terminate the buffer
-	std::cout << "Received message: " << buffer << std::endl;
+	std::cout << "Received message: " << buffer << std::endl; */
 	//________END TESTING BLOCK_________
 
 	while (Server::signalReceived == false)
 	{
-		//poll clients fds
+		if(poll(&fds[0],fds.size(),-1) == -1)//3rd arg of poll() is the timeout in milliseconds (-1 means infinite timeout, that is to say, poll will block indefinitely until an event occurs!!!!)
+			throw(std::runtime_error("server could not poll sockets fd's"));
+		for (size_t i = 0; i < this->fds.size(); i++)
+		{
+			if (fds[i].revents & POLLIN)//bitwise AND - if evaluates to true only when a POLLIN event occurred on this fd
+			{
+				if (fds[i].fd == listenSocketFd)//socket with event is the server's listen socket, so there is an incoming connection
+					this->accept_new_client();
+				//else
+					//read data from client
+			}
+		}
 	}
 	this->closeFds();
+}
+void Server::accept_new_client()
+{
+	//Client cli;
+	memset(&clientAddress, 0, sizeof(clientAddress));
+	socklen_t clientAddressLength = sizeof(clientAddress);
+	int connectedSocketFd = accept(this->listenSocketFd, (sockaddr *)&(clientAddress), &clientAddressLength);
+	if (connectedSocketFd == -1)
+		{
+			std::cout << "server could not accept incoming connection" << std::endl; 
+			return;
+		}
+	if (fcntl(connectedSocketFd, F_SETFL, O_NONBLOCK) == -1)
+		{
+			std::cout << "fcntl() failed" << std::endl;
+			return;
+		}
+	this->connectedSocket.fd = connectedSocketFd;
+	this->connectedSocket.events = POLLIN;
+	this->connectedSocket.revents = 0;
+	this->fds.push_back(this->connectedSocket);
+/* 	cli.SetFd(incofd);
+	cli.setIpAdd(inet_ntoa((cliadd.sin_addr)));
+	clients.push_back(cli); */
+	
+	std::cout << "a new client has been connected with socket fd " << connectedSocketFd << std::endl;
 }
