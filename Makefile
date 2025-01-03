86186aab2e11df37d1dd7c01ac7526d9bc9e9999
nateto <nateto@hotmail.com>
2025-01-03 16:42:08 +0100

    simple client added for testing purposes

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: igcastil <igcastil@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/28 14:26:37 by igcastil          #+#    #+#              #
-#    Updated: 2024/12/25 23:56:05 by igcastil         ###   ########.fr        #
+#    Updated: 2025/01/03 16:39:50 by igcastil         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,11 +18,15 @@ all: $(NAME)
 
 $(NAME):
 	$(CC) $(FLAGS) main.cpp src/Server.cpp -o $(NAME)
+
+client:
+	$(CC) $(FLAGS) test_client.cpp -o client
 clean:
 	rm -rf $(NAME)
 
 fclean:
 	rm -rf $(NAME)
+	rm -rf client
 
 re: clean all
 
---
+++ test_client.cpp
@@ -0,0 +1,44 @@
+#include <sys/socket.h>
+#include <cstdio>
+#include <cstdlib>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <unistd.h>
+
+int main()
+{
+	int					sockfd;
+	char				input[4096 + 1];
+	struct sockaddr_in	servaddr;
+
+	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+		printf("socket error");
+        exit(1);
+    }
+
+	memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+	servaddr.sin_port   = htons(1234);
+	if (inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr) <= 0)
+    {
+		printf("inet_pton error");
+        exit(1);
+    }
+	if (connect(sockfd, (sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+		printf("connect error");
+        exit(1);
+    }
+	while (1) {
+        if (fgets(input, sizeof(input), stdin) == NULL)
+            break;
+		if (write(sockfd, input, strlen(input)) < 0)
+		{
+			printf("write error");
+			exit(1);
+		}
+	}
+	exit(0);
+}
