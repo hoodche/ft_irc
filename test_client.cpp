c33e888bc3d3fd98777cd882e9291e1e6913dfd9
nateto <nateto@hotmail.com>
2025-01-03 16:52:27 +0100

    comments

--- test_client.cpp
+++ test_client.cpp
@@ -12,29 +12,29 @@ int main()
 	char				input[4096 + 1];
 	struct sockaddr_in	servaddr;
 
-	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)//create socket
     {
 		printf("socket error");
         exit(1);
     }
 
 	memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
+    servaddr.sin_family = AF_INET;//we now write the struct with server´s ip address and port
 	servaddr.sin_port   = htons(1234);
 	if (inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr) <= 0)
     {
 		printf("inet_pton error");
         exit(1);
     }
-	if (connect(sockfd, (sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+	if (connect(sockfd, (sockaddr *)&servaddr, sizeof(servaddr)) < 0)//connect socket to server
     {
 		printf("connect error");
         exit(1);
     }
 	while (1) {
-        if (fgets(input, sizeof(input), stdin) == NULL)
+        if (fgets(input, sizeof(input), stdin) == NULL)//get what is typed in terminal
             break;//client finished with  CTRL + D
-		if (write(sockfd, input, strlen(input)) < 0)
+		if (write(sockfd, input, strlen(input)) < 0)//write what was typed in terminal to connected socket
 		{
 			printf("write error");
 			exit(1);
