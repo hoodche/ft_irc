6d03c1e4f86a177f2d2df6f1ef26d3a5feab8e04
Nerea <nervilbar@gmail.com>
2025-01-04 19:13:01 +0100

    client updated with getter, setters and iter function

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -4,22 +4,33 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
 
 class Client {
 	private:
 		int 		fd;
 		bool		registered;
+		bool		oper;
 		std::string	nick;
 		std::string	username;
-		std::string	password;
+		std::string	password; // Needed?
 
 	public:
 		Client(void);
 		Client(int receivedFd);
 		~Client(void);
 
-		// getter
+		// Getters
 		int	getSocketFd(void) const;
+		std::string	getNickname(void) const;
+		std::string getUsername(void) const;
+
+		// Setters
+		void setNickname(std::string nickname);
+		void setUsername(std::string user);
+
+		// Methods
+		static Client *findClientByFd(int fd, std::vector<Client> &clients);
 };
 
 #endif
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,12 +1,39 @@
 
 #include "../inc/Client.hpp"
 
-Client::Client(void): fd(-1), registered(false) {}
+Client::Client(void): fd(-1), registered(false), oper(false), nick(""), username("") {}
 
-Client::Client(int receivedFd): fd(receivedFd), registered(false) {}
+Client::Client(int receivedFd): fd(receivedFd), registered(false), oper(false), nick(""), username("") {}
 
 Client::~Client(void) {}
 
 int Client::getSocketFd(void) const {
-	return fd;
+	return this->fd;
+}
+
+std::string Client::getNickname(void) const {
+	return this->nick;
+}
+
+std::string Client::getUsername(void) const {
+	return this->username;
+}
+
+void Client::setNickname(std::string nickname) {
+	nick = nickname;
+	return ;
+}
+
+void Client::setUsername(std::string user) {
+	username = user;
+	return ;
+}
+
+Client* Client::findClientByFd(int fd, std::vector<Client> &clients) {
+    for (size_t i = 0; i < clients.size(); i++) {
+        if (clients[i].getSocketFd() == fd) {
+            return &clients[i];
+        }
+    }
+    return NULL;
 }
