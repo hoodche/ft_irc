16b1796cadb3827ccd445934676be1b643cf44cc
fmoran-m <franmoranmato@gmail.com>
2025-02-10 13:58:37 +0100

    commit fixing topic

---
+++ main.d
@@ -0,0 +1,9 @@
+main.o: main.cpp inc/Server.hpp inc/../inc/Client.hpp \
+ inc/../inc/Handler.hpp inc/../inc/../inc/Client.hpp \
+ inc/../inc/../inc/Channel.hpp inc/../inc/../inc/../inc/Client.hpp
+inc/Server.hpp:
+inc/../inc/Client.hpp:
+inc/../inc/Handler.hpp:
+inc/../inc/../inc/Client.hpp:
+inc/../inc/../inc/Channel.hpp:
+inc/../inc/../inc/../inc/Client.hpp:
