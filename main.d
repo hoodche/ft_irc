e9cf2d38c128b588035da8964a35786d972f3127
fmoran-m <franmoranmato@gmail.com>
2025-02-10 14:13:17 +0100

    fix: topic not set show in client

--- main.d
+++
@@ -1,9 +0,0 @@
-main.o: main.cpp inc/Server.hpp inc/../inc/Client.hpp \
- inc/../inc/Handler.hpp inc/../inc/../inc/Client.hpp \
- inc/../inc/../inc/Channel.hpp inc/../inc/../inc/../inc/Client.hpp
-inc/Server.hpp:
-inc/../inc/Client.hpp:
-inc/../inc/Handler.hpp:
-inc/../inc/../inc/Client.hpp:
-inc/../inc/../inc/Channel.hpp:
-inc/../inc/../inc/../inc/Client.hpp:
